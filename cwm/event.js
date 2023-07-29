85536599356b178bb6cba1472f0159b6d6718d0a
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-07-28 22:12:35 -0700

    added more code examples to event.js file

--- cwm/event.js
+++ cwm/event.js
@@ -2,9 +2,9 @@ let EventEmiter = require('events')
 
 const emitter = new EventEmiter()
 //registere a listener
-emitter.on("messageLogged",()=>{
-    console.log("emitter is listening")
+emitter.on("messageLogged",(args)=>{
+    console.log("emitter is listening",args.id)
 })
-emitter.emit("messageLogged")
+emitter.emit("messageLogged",{id:1,url:'https://'})
 //emit means making a noise or rising something
 
